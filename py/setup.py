deaf5a99e78e313fd3ce6550454a88becc08a1af
g4z3 <g4z3g4z3@gmail.com>
2017-11-08 19:07:01 +0800

    Add singleton

---
+++ py/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+#import git
+
+#repo = git.Repo('.', search_parent_directories=True)
+setup(
+    name         = 'g4z3',
+    description  = "g4z3's tool kit",
+    author       = 'g4z3',
+    version      = '0.1',
+    packages     = ['g4z3kit']
+)
